9a437a45cf46f36a127669518ed11fc00ab040b0
Sarthak Roy <sarthakroy2002@gmail.com>
2022-10-23 04:00:46 +0000

    sm8250-common: Build android.hardware.secure_element@1.0
    10-22 21:47:20.138  4080  4080 F linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/vendor.qti.secure_element@1.2-service": library "android.hardware.secure_element@1.0.so" not found: needed by main executable
    10-22 21:47:16.295  3844  3844 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.secure_element@1.0.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)
    
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- kona.mk
+++ kona.mk
@@ -435,6 +435,11 @@ PRODUCT_PACKAGES += \
     init.xiaomi.rc \
     ueventd.qcom.rc
 
+# Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.2.vendor
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
