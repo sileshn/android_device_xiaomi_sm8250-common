373ef7d35063af8c0927846abbbb85575080be2c
¨raystef66¨ <s.vanbarel@gmail.com>
2022-12-14 09:30:57 +0100

    sm8250-common: add audio.usb.default package

--- kona.mk
+++ kona.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.primary.kona \
     audio.r_submix.default \
-    audio.usbv2.default \
+    audio.usb.default \
+    audio.usbv2.default
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
