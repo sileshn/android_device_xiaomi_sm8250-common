d99704070ebdfd02653a9f070e590103d1ab1630
Chenyang Zhong <zhongcy95@gmail.com>
2023-03-14 03:05:27 +0600

    sm8250-common: init: Avoid migrating foreground tasks to X1
    The Cortex-X1 core is quite power hungry. Let's only use it for
    top-app tasks. Avoid migrating foreground tasks to X1 to reduce
    unnecessary bumps of X1's frequency and save some battery.
    
    Change-Id: I8bb7655010da98c083560d647d5c682cd636cc82
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5741,7 +5741,7 @@ case "$target" in
         echo 0-2     > /dev/cpuset/background/cpus
         echo 0-3     > /dev/cpuset/system-background/cpus
         echo 4-7     > /dev/cpuset/foreground/boost/cpus
-        echo 0-2,4-7 > /dev/cpuset/foreground/cpus
+        echo 0-2,4-6 > /dev/cpuset/foreground/cpus
         echo 0-7     > /dev/cpuset/top-app/cpus
 
 	# Turn off scheduler boost at the end
